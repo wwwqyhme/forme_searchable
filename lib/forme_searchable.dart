2385d94fda6b1ff9fa7d833fd3d00ccc66d1ba83
wwwqyhme <wwwqyhme@gmail.com>
2022-01-19 16:59:13 +0800

    1.0.0

--- lib/forme_searchable.dart
+++ lib/forme_searchable.dart
@@ -1,4 +1,5 @@
 export 'src/field/material/dialog_configuration.dart';
+export 'src/field/material/forme_default_searchable_content.dart';
 export 'src/field/material/forme_page_result.dart';
 export 'src/field/material/forme_searchable.dart';
 export 'src/field/material/forme_searchable_content.dart';
